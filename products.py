22d17443c7aa1c1fcd5503009a9b8c1d20c6f159
Jeff Kee <jeffkhm@gmail.com>
2020-06-03 20:16:58 -0700

    add for loop for printing products

--- products.py
+++ products.py
@@ -5,8 +5,10 @@ while True:
         break
     price = input('请输入厂品价格：')
     p = []
-    p.append(name)
+    p.append(name) # comprehensive list "p = [name, price]" = p=[];p.append(name);p.append(price)
     p.append(price)
     products.append(p) #把 p 加入 products 大清单
 print(products)    
 
+for product in products:   # for loop 的定义是 products 中一个一个那出来
+	print(product[0], '的价格是', product[1])
