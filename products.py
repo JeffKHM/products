1431d8d171f13ba89d744943823fc68783b4679b
Jeff Kee <jeffkhm@gmail.com>
2020-06-06 01:10:26 -0700

    add read file

--- products.py
+++ products.py
@@ -2,12 +2,13 @@
 products = []
 with open('products.csv', 'r', encoding='utf-8') as f:
     for line in f:
-    	if '商品, 价格' in line:
-    		continue # 继续 如果在 line 里面就跳下一回继续
+        if '商品,价格' in line:# if '商品,价格' in line:
+            continue  #   continue # 继续 如果在 line 里面就跳下一回继续
         name, price = line.strip().split(',')
-        products.append([name, price]) 
-print(products) 
-        
+        products.append([name, price])
+
+print(products)
+
 #让使用者输入
 while True:
     name = input('请输入产品名称：')
@@ -21,14 +22,16 @@ print(products)
 
 # 印出所有购买今纪录
 for p in products:   # for loop 的定义是 products 中一个一个那出来
-	print(p[0], '的价格是', p[1])
+    print(p[0], '的价格是', p[1])
 
 # 写入档案
-with open('products.csv', 'w', encoding ='utf-8') as f: # write (w) 写入模式, 如果没有 products.txt or csv file 没关系
-	f.write('商品, 价格 \n')
-	for p in products:          # read (r) 读取模式
-		f.write(p[0] + ',' + str(p[1]) + '\n') #.write 是写入
+with open('products.csv', 'w', encoding='utf-8') as f: # write (w) 写入模式, 如果没有 products.txt or csv file 没关系
+    f.write('商品,价格\n')
+    for p in products:          # read (r) 读取模式
+        f.write(p[0] + ',' + str(p[1]) + '\n') #.write 是写入
 
 
 
 
+
+       
